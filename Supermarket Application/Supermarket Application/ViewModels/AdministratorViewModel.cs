3e8e6c7e069cb7e09ddfe29542d568e9aea119ec
andreeamacsim <mlaviniaandreea@yahoo.com>
2024-05-02 19:01:59 +0300

    I made the Admin dashboard and I created another 3 xaml files for the possibility to add, update and 'delete' a product.

---
+++ Supermarket Application/Supermarket Application/ViewModels/AdministratorViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Supermarket_Application.Views;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Supermarket_Application.ViewModels
+{
+    public class AdministratorViewModel : INotifyPropertyChanged
+    {
+        public ICommand OpenAddProductCommand { get; private set; }
+        public ICommand OpenUpdateProductPriceCommand { get; private set; }
+        public ICommand OpenLogicalDeletionCommand { get; private set; }
+
+        public AdministratorViewModel()
+        {
+            OpenAddProductCommand = new RelayCommand(OpenAddProduct);
+            OpenUpdateProductPriceCommand = new RelayCommand(OpenUpdateProductPrice);
+            OpenLogicalDeletionCommand = new RelayCommand(OpenLogicalDeletion);
+        }
+
+        private void OpenAddProduct(object obj)
+        {
+            AddProductView addProductView = new AddProductView();
+            addProductView.ShowDialog();
+        }
+
+        private void OpenUpdateProductPrice(object obj)
+        {
+            UpdateProductPriceView updateProductPriceView = new UpdateProductPriceView();
+            updateProductPriceView.ShowDialog();
+        }
+
+        private void OpenLogicalDeletion(object obj)
+        {
+            LogicalDeletionView logicalDeletionView = new LogicalDeletionView();
+            logicalDeletionView.ShowDialog();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
